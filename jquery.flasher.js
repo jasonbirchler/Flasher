e129d76637187cd48c022e36b1e74e05fedb50bf
Jason Birchler <jasonbirchler@gmail.com>
2013-04-28 15:52:34 -0700

    refactoring plugin code to follow best practices per http://docs.jquery.com/Plugins/Authoring

--- jquery.flasher.js
+++ jquery.flasher.js
@@ -1,28 +1,41 @@
-(function($) {
-    jQuery.fn.flasher = function (msg, options) {
-        var defaults = {
-            message   : msg, // because a message is required
-                             // it's not passed as an option
-            event     : 'click', // event to listen for
-            $el       : '<div class="flasher"/>',
-            animation : '', // animation used to show/hide the flash
-            duration  : 1000 // how long the flash displays in miliseconds
-        };
-        var o = $.extend(defaults, options);
+(function($) {        
+    var methods = {
+        init : function (options) {
+            var defaults = {
+                event     : 'click', // event to listen for
+                $el       : '<div class="flasher"/>',
+                animation : '', // animation used to show/hide the flash
+                duration  : 1000 // how long the flash displays in miliseconds
+            };
+            var o = $.extend(defaults, options);
 
-        return this.each(function(index) {
-            var e = $(this);
+            return this.each(function(index) {
+                var $this = $(this);
 
-            // insert the message into $el
+                // insert the message into $el
 
-            e.on(o.event, function () {
-                console.log(o.message);
-                // show the flash message
-            })
+                $this.on(o.event, function () {
+                    console.log(o.message);
+                    // show the flash message
+                })
+            });
+        },
+        destroy : function (args) {
+            return this.each(function(index) {
+                console.log("destroyed!");
+            });
+        }
+    };
 
-            // show method
+    $.fn.flasher = function( method ) {
 
-            //destroy method
-        });
-    }
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+        }    
+
+    };
 })(jQuery)
